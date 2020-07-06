5da275bd62c3eaad1a24a5806d29e6519b052f64
Timothy Cole <tim@timcole.me>
2020-07-05 18:53:10 -0700

    Added Screenshot scripts

---
+++ nuuls.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+MAIN_FOLDER="$HOME/Pictures/Screenshots"
+NAME=$(openssl rand -base64 16 | tr -dc '[:alnum:]\n\r')
+
+# Make folder(s)
+mkdir -p ${MAIN_FOLDER}/
+
+# Take Screenshot
+screencapture -x -s -C -m ${MAIN_FOLDER}/${NAME}.png
+
+# Send Uploading Notification
+osascript -e 'display notification "'"$NAME"'" with title "Uploading Screenshot"'
+
+# Upload Screenshot
+FILE_URL=$(curl -F "file=@${MAIN_FOLDER}/${NAME}.png" https://i.nuuls.com/upload -s)
+
+# Copy URL to clipboard
+echo $FILE_URL | pbcopy 
+
+# Send Upload Notification
+osascript -e 'display notification "'"$FILE_URL"'" with title "Uploaded Screenshot"'
---
+++ screenshot.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+MAIN_FOLDER="$HOME/Pictures/Screenshots"
+BUCKET_NAME="cdn.tcole.me"
+NAME=$(openssl rand -base64 16 | tr -dc '[:alnum:]\n\r')
+GSUTIL_PATH="/usr/local/bin/gsutil"
+
+# Make folder
+mkdir -p ${MAIN_FOLDER}/
+
+# Take Screenshot
+screencapture -x -s -C -m ${MAIN_FOLDER}/${NAME}.png
+
+if [ ! -e ${MAIN_FOLDER}/${NAME}.png ]; then exit 0; fi
+
+# Send Uploading Notification
+osascript -e 'display notification "'"$NAME"'" with title "Uploading Screenshot"'
+
+# Upload
+$GSUTIL_PATH cp ${MAIN_FOLDER}/${NAME}.png gs://${BUCKET_NAME}/${NAME}.png
+
+# File URL
+FILE_URL="https://timcole.me/ss/${NAME}.png"
+
+# Copy URL to clipboard
+echo $FILE_URL | pbcopy 
+
+# Send Upload Notification
+osascript -e 'display notification "'"$FILE_URL"'" with title "Uploaded Screenshot"'
